c061f74b973cb8dc72fadd457f94527b6b67c201
wiemfeddaoui <wiemfeddaoui99@gmail.com>
2024-07-29 01:37:05 +0100

    fixed bug in foodcatalogue service

--- foodcatalogue/src/main/java/com/myproject/foodcatalogue/service/FoodCatalogueService.java
+++ foodcatalogue/src/main/java/com/myproject/foodcatalogue/service/FoodCatalogueService.java
@@ -5,6 +5,7 @@ import com.myproject.foodcatalogue.mapper.FoodItemMapper;
 import com.myproject.foodcatalogue.repo.FoodItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.myproject.foodcatalogue.entity.FoodItem;
 
 @Service
 public class FoodCatalogueService {
